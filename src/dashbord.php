8a128657c4efc2d54189e654a89067089da99a88
nazi sharifi <banonazisharifi@gmail.com>
2025-09-21 22:14:58 +0430

    l

--- src/dashbord.php
+++ src/dashbord.php
@@ -4,8 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="../public/output.css">
 </head>
 <body>
-    
+    <div class="h-screen w-full bg-stone-50 grid grid-cols-3">
+        <div class="col-span-1 bg-blue-950 text-white">
+            <div class="px-5 py-4 bg-amber-600 flex justify-between items-center">
+                <img src="../images/logo.png"  alt="">
+                <i class="fas fa-bars text-white text-3xl"></i>
+            </div>
+            <!-- logo -->
+             <div class="bg-gray-950 py-6 px-6 flex hover:cursor-pointer justify-between items-center">
+                <div class="flex items-center space-x-6 ">
+                    <i class="fa fa-tachometer-alt text-2xl text-amber-600"></i>
+                    <span class="text-gray-300 text-xl">Dashbord</span>
+                </div>
+                <i class="fas fa-chevron-right text-2xl"></i>
+             </div>
+        </div>
+        <div class="col-span-2 bg-blue-500"></div>
+    </div>
 </body>
 </html>
